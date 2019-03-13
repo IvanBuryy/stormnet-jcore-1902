c2def26bad024d27e9c3229a61c8b54014edc2b2
Иван <ivan.bury88@gmail.com>
2019-03-13 23:36:35 +0300

    IfElsseTask - end

--- src/by/stormnet/task/conditionals/IfElseTask.java
+++ src/by/stormnet/task/conditionals/IfElseTask.java
@@ -11,7 +11,10 @@ class IfElseTask {
    * то следует вернуть разность между x и y.
    */
   int ifElse(int x, int y) {
-    throw new TaskNotImplementedException();
+    if (x*y>10)
+    {return 42;}
+  else
+    {return x-y;}
   }
 
   /**
@@ -22,7 +25,13 @@ class IfElseTask {
    *   - на 4, если x больше либо равно 15 и y больше 15.
    */
   int ifElseIf(float x, float y) {
-    throw new TaskNotImplementedException();
+    if (x<10)
+    {return (int)(x/y)*2;}
+    else if (x>=10&&x<15)
+    {return (int)(x/y)*3;}
+    else if (x>=15&&y>15)
+    {return (int)(x/y)*4;}
+    return 0;
   }
 
   /**
@@ -33,6 +42,11 @@ class IfElseTask {
    * Если не выполняется ни одно из условий, то нужно вернуть исходное значение x.
    */
   int ifElseIfElse(int x) {
-    throw new TaskNotImplementedException();
+    if(x%2==0)
+    {return x*2;}
+    if(x%3==0)
+    {return x*3;}
+    else
+    {return x;}
   }
 }
